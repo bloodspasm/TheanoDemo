72b7e277c5d34c36f55cce7be41b353caf0cc416
fox <chlliao@yaduo.com>
2017-02-22 14:52:10 +0800

    激励函数

---
+++ theano3.py
@@ -0,0 +1,11 @@
+""""
+
+在theano中可用的激活功能可以在这个链接中找到：
+http://deeplearning.net/software/theano/library/tensor/nnet/nnet.html
+激活功能包括但不限于softplus，sigmoid，relu，softmax，elu，tanh
+对于隐藏层，我们可以使用relu，tanh，softplus ...
+对于分类问题，我们可以使用sigmoid或softmax作为输出层。
+对于回归问题，我们可以对输出层使用线性函数。
+
+"""
+
