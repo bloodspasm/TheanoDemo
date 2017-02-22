8b3afc27ea96aa07162ff6a4818f3bc66c349b55
fox <chlliao@yaduo.com>
2017-02-22 11:13:36 +0800

    function

--- theano1.py
+++ theano1.py
@@ -6,9 +6,9 @@ from theano import function
 x = T.dscalar('x');
 y = T.dscalar('y');
 z = x + y;
-f = function([x,y],z)
+f = function([x, y], z)
 
-print f(2,3)
+print f(2, 3)
 
 # 显示出z的函数
 
@@ -16,30 +16,42 @@ from theano import pp
 
 print pp(z)
 
-#做矩阵的运算
+# 做矩阵的运算
 
-x = T.dmatrix('x') #float 64位
+x = T.dmatrix('x')  # float 64位
 y = T.dmatrix('y')
-z = x+y # T.dot(x,y) 乘法
-f = function([x,y],z)
+z = x + y  # T.dot(x,y) 乘法
+f = function([x, y], z)
 
-print f(np.arange(12).reshape((3,4)),10*np.ones((3,4)))
+print f(np.arange(12).reshape((3, 4)), 10 * np.ones((3, 4)))
 
 print '#################################'
 
 import theano
+
 # 激励函数
 
 x = T.dmatrix('x')
-s = 1/(1+T.exp(-x)) # np.exp() logsistic or step
-logsistic = theano.function([x],s)
-print logsistic([[0,1],[-2,-3]])
-print '#################################'
-a,b = T.dmatrices('a','b')
+s = 1 / (1 + T.exp(-x))  # np.exp() logsistic or step
+logsistic = theano.function([x], s)
+print (logsistic([[0, 1], [-2, -3]]))
+print ('#################################')
+a, b = T.dmatrices('a', 'b')
 diff = a - b
 abs_diff = abs(diff)
-diff_squared = diff**2
-f = theano.function([a,b],[diff,abs_diff,diff_squared])
+diff_squared = diff ** 2
+f = theano.function([a, b], [diff, abs_diff, diff_squared])
 
-print f(np.ones((2,2)),np.arange(4).reshape((2, 2)))
-print '#################################'
+print (f(np.ones((2, 2)), np.arange(4).reshape((2, 2))))
+print ('#################################')
+
+x, y, w = T.dscalars('x', 'y', 'w')
+z = (x + y) * w
+f = theano.function([x,
+                     theano.In(y, value=1),
+                     theano.In(w, value=2, name='weights')],
+                    z)
+print(f(23, 2, weights=10))
+# #等价于
+# def f(a,b = 1,c =2)
+#     return (a+b)*c
